8f52739cfb30e201f270336b89da2dec24e08a82
shahria12366 <121188067+shahria12366@users.noreply.github.com>
2025-07-10 15:15:07 +0400

    Update sales_performance_dashboard.py

--- sales_performance_dashboard.py
+++ sales_performance_dashboard.py
@@ -23,7 +23,7 @@ def format_num(num):
 
 def main():
 
-    df = pd.read_csv("data_for_dashboard_v1 copy.csv")
+    df = pd.read_csv("data_for_dashboard.csv")
     df = df[df['outlier_flag']=='No']
     with st.sidebar:
         st.title("Bayut and Dubizzle Listings Dashboard")
